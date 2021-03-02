d3ff5689a0ec1a7b57325a7756d1490322faf1b8
Vault <62789852+VaultGitos@users.noreply.github.com>
2021-03-02 22:08:14 +0700

    Create Loader.lua

---
+++ Games/Loader.lua
@@ -0,0 +1,22 @@
+local gamelist = {
+    [2041312716] = "CatdollEngine",
+    [1962086868] = "TowerOfMeow",
+    [5977280685] = "CattoLegends",
+    [537413528] = "BABFT",
+    [4169490976] = "MortemMecattum",
+    [914010731] = "CatGhoul"
+}
+
+for gameId,CatWare in pairs(gamelist) do
+    if gameId == game.PlaceId then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/CatzCode/cat/main/Games/'..CatWare..'.lua'))()
+    end
+end
+
+spawn(function()
+    local VirtualUser=game:GetService'VirtualUser'
+    game:GetService'Players'.LocalPlayer.Idled:Connect(function()
+        VirtualUser:CaptureController()
+        VirtualUser:ClickButton2(Vector2.new())
+    end)
+end)
