ffb6240bffa85e177371bd9df45c3656acb4945f
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2021-05-01 02:08:27 +0200

    Update PhantomForces-LibrariesAndFramework.lua

--- Library/PhantomForces-LibrariesAndFramework.lua
+++ Library/PhantomForces-LibrariesAndFramework.lua
@@ -13,60 +13,62 @@ local getLibraries = function()
 end
 local getFramework = function()
     local framework = { }
+    local success = true
     local tries = 0
     repeat
         if tries == 10 then
             return { }, false -- cri
         end
-        
         tries = tonumber(tries) + 1
 
-        for i,v in pairs(getgc(true)) do
-            if type(v) == "function" then
-                local function_name = debug.getinfo(v).name:lower()
-                if function_name == "trajectory" and not framework.trajectory then
-                    framework.trajectory = v
+        success = pcall(function()
+            for i,v in pairs(getgc(true)) do
+                if type(v) == "function" then
+                    local function_name = debug.getinfo(v).name:lower()
+                    if function_name == "trajectory" and not framework.trajectory then
+                        framework.trajectory = v
+                    end
+                    if function_name == "getgunlist" and not framework.getgunlist then
+                        framework.getgunlist = v
+                    end
                 end
-                if function_name == "getgunlist" and not framework.getgunlist then
-                    framework.getgunlist = v
+                if type(v) == "table" then
+                    if rawget(v, "step") and rawget(v, "reset") and rawget(v, "new") and not framework.sound then
+                        framework.particle = v
+                    end 
+                    if rawget(v, "PlaySoundId") and not framework.sound then
+                        framework.sound = v
+                    end 
+                    if rawget(v, "setmovementmode") and not framework.character then
+                        framework.character = v
+                    end    
+                    if rawget(v, "send") and not framework.network then
+                        framework.network = v
+                    end
+                    if rawget(v, "gammo") and not framework.gamelogic then
+                        framework.gamelogic = v
+                    end
+                    if rawget(v, "basecframe") and not framework.camera then
+                        framework.camera = v
+                    end
+                    if rawget(v, "breakwindow") and not framework.effects then
+                        framework.effects = v
+                    end
+                    if rawget(v, "firehitmarker") and not framework.uieffects then
+                        framework.uieffects = v
+                    end
+                    if rawget(v, "getbodyparts") and not framework.replication then
+                        framework.replication = v
+                    end
+                    if rawget(v, "bulletLifeTime") and not framework.publicsettings then
+                        framework.publicsettings = v
+                    end
                 end
             end
-            if type(v) == "table" then
-                if rawget(v, "step") and rawget(v, "reset") and rawget(v, "new") and not framework.sound then
-                    framework.particle = v
-                end 
-                if rawget(v, "PlaySoundId") and not framework.sound then
-                    framework.sound = v
-                end 
-                if rawget(v, "setmovementmode") and not framework.character then
-                    framework.character = v
-                end    
-                if rawget(v, "send") and not framework.network then
-                    framework.network = v
-                end
-                if rawget(v, "gammo") and not framework.gamelogic then
-                    framework.gamelogic = v
-                end
-                if rawget(v, "basecframe") and not framework.camera then
-                    framework.camera = v
-                end
-                if rawget(v, "breakwindow") and not framework.effects then
-                    framework.effects = v
-                end
-                if rawget(v, "firehitmarker") and not framework.uieffects then
-                    framework.uieffects = v
-                end
-                if rawget(v, "getbodyparts") and not framework.replication then
-                    framework.replication = v
-                end
-                if rawget(v, "bulletLifeTime") and not framework.publicsettings then
-                    framework.publicsettings = v
-                end
-            end
-        end
+        end)
         wait(1)
     until (framework.trajectory and framework.getgunlist and framework.particle and framework.sound and framework.character and framework.network and framework.gamelogic and framework.camera and framework.effects and framework.uieffects and framework.replication and framework.publicsettings)
 
-    return framework, true
+    return framework, success
 end
 return getLibraries, getFramework
