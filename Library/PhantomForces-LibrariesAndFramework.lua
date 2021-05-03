64688e64e6429268afd713cf8b73ea0a8b290aca
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2021-05-03 12:18:57 +0200

    Update PhantomForces-LibrariesAndFramework.lua

--- Library/PhantomForces-LibrariesAndFramework.lua
+++ Library/PhantomForces-LibrariesAndFramework.lua
@@ -19,42 +19,42 @@ local getFramework = function()
         for i,v in pairs(getgc(true)) do
             if type(v) == "function" then
                 local function_name = debug.getinfo(v).name:lower()
-                if function_name == "trajectory" and not framework.trajectory then
+                if function_name == "trajectory" then
                     framework.trajectory = v
                 end
-                if function_name == "getgunlist" and not framework.getgunlist then
+                if function_name == "getgunlist" then
                     framework.getgunlist = v
                 end
             end
             if type(v) == "table" then
-                if rawget(v, "step") and rawget(v, "reset") and rawget(v, "new") and not framework.sound then
+                if rawget(v, "step") and rawget(v, "reset") and rawget(v, "new") then
                     framework.particle = v
                 end 
-                if rawget(v, "PlaySoundId") and not framework.sound then
+                if rawget(v, "PlaySoundId") then
                     framework.sound = v
                 end 
-                if rawget(v, "setmovementmode") and not framework.character then
+                if rawget(v, "setmovementmode") then
                     framework.character = v
                 end    
-                if rawget(v, "send") and not framework.network then
+                if rawget(v, "send") then
                     framework.network = v
                 end
-                if rawget(v, "gammo") and not framework.gamelogic then
+                if rawget(v, "gammo") then
                     framework.gamelogic = v
                 end
-                if rawget(v, "basecframe") and not framework.camera then
+                if rawget(v, "basecframe") then
                     framework.camera = v
                 end
-                if rawget(v, "breakwindow") and not framework.effects then
+                if rawget(v, "breakwindow") then
                     framework.effects = v
                 end
-                if rawget(v, "firehitmarker") and not framework.uieffects then
+                if rawget(v, "firehitmarker") then
                     framework.uieffects = v
                 end
-                if rawget(v, "getbodyparts") and not framework.replication then
+                if rawget(v, "getbodyparts") then
                     framework.replication = v
                 end
-                if rawget(v, "bulletLifeTime") and not framework.publicsettings then
+                if rawget(v, "bulletLifeTime") then
                     framework.publicsettings = v
                 end
             end
