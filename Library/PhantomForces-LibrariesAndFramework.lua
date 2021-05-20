b06c38e0ee56870bcd024c5641d0236c95d3b943
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2021-05-20 13:02:58 +0200

    Update PhantomForces-LibrariesAndFramework.lua

--- Library/PhantomForces-LibrariesAndFramework.lua
+++ Library/PhantomForces-LibrariesAndFramework.lua
@@ -14,41 +14,46 @@ end
 local getFramework = function()
     local framework = { }
     local success = pcall(function()
-        for i,v in pairs(getgc(true)) do
-            if type(v) == "function" and debug.getinfo(v).name:lower() == "getgunlist" then
-                framework.getgunlist = v
+        local tries = 0
+        repeat
+            tries = tries + 1
+            for i,v in pairs(getgc(true)) do
+                if type(v) == "function" and debug.getinfo(v).name:lower() == "getgunlist" then
+                    framework.getgunlist = v
+                end
+                if type(v) == "table" and rawget(v, "step") and rawget(v, "reset") and rawget(v, "new") then
+                    framework.particle = v
+                end 
+                if type(v) == "table" and rawget(v, "PlaySoundId") then
+                    framework.sound = v
+                end 
+                if type(v) == "table" and rawget(v, "setmovementmode") then
+                    framework.character = v
+                end    
+                if type(v) == "table" and rawget(v, "send") then
+                    framework.network = v
+                end
+                if type(v) == "table" and rawget(v, "gammo") then
+                    framework.gamelogic = v
+                end
+                if type(v) == "table" and rawget(v, "basecframe") then
+                    framework.camera = v
+                end
+                if type(v) == "table" and rawget(v, "breakwindow") then
+                    framework.effects = v
+                end
+                if type(v) == "table" and rawget(v, "firehitmarker") then
+                    framework.uieffects = v
+                end
+                if type(v) == "table" and rawget(v, "getbodyparts") then
+                    framework.replication = v
+                end
+                if type(v) == "table" and rawget(v, "bulletLifeTime") then
+                    framework.publicsettings = v
+                end
             end
-            if type(v) == "table" and rawget(v, "step") and rawget(v, "reset") and rawget(v, "new") then
-                framework.particle = v
-            end 
-            if type(v) == "table" and rawget(v, "PlaySoundId") then
-                framework.sound = v
-            end 
-            if type(v) == "table" and rawget(v, "setmovementmode") then
-                framework.character = v
-            end    
-            if type(v) == "table" and rawget(v, "send") then
-                framework.network = v
-            end
-            if type(v) == "table" and rawget(v, "gammo") then
-                framework.gamelogic = v
-            end
-            if type(v) == "table" and rawget(v, "basecframe") then
-                framework.camera = v
-            end
-            if type(v) == "table" and rawget(v, "breakwindow") then
-                framework.effects = v
-            end
-            if type(v) == "table" and rawget(v, "firehitmarker") then
-                framework.uieffects = v
-            end
-            if type(v) == "table" and rawget(v, "getbodyparts") then
-                framework.replication = v
-            end
-            if type(v) == "table" and rawget(v, "bulletLifeTime") then
-                framework.publicsettings = v
-            end
-        end
+            wait(1)
+        until tries >= 10 or (framework.getgunlist and framework.particle and framework.sound and framework.character and framework.network and framework.gamelogic and framework.camera and framework.effects and framework.uieffects and framework.replication and framework.publicsettings)
     end)
     return framework, success and (framework.getgunlist and framework.particle and framework.sound and framework.character and framework.network and framework.gamelogic and framework.camera and framework.effects and framework.uieffects and framework.replication and framework.publicsettings)
 end
