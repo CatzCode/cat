4a91a6760de6426de3dec913661a84ac795679ae
Vault <62789852+VaultGitos@users.noreply.github.com>
2021-08-15 16:02:35 +0700

    Update Framework.lua

--- Libraries/Mortem/Framework.lua
+++ Libraries/Mortem/Framework.lua
@@ -1,7 +1,36 @@
 getgenv().Framework = {
     getpos = "Head",
-    Enabled = false
+    Enabled = false,
+    VisiblityCheck = false,
 }
+
+local lplr = game.Players.LocalPlayer
+local mouse = game.Players.LocalPlayer:GetMouse()
+
+local function IsVisible(pos, ignoreList)
+    return #workspace.CurrentCamera:GetPartsObscuringTarget({game.Players.LocalPlayer.Character.Head.Position, pos}, ignoreList) == 0 and true or false
+end
+
+function nearest()
+    local target = nil
+    local dist = math.huge
+    for i, v in pairs(game.Players:GetPlayers()) do
+        if v.Name ~= game.Players.LocalPlayer.Name then
+            if v.Character and v.Character:FindFirstChild("Humanoid") and v.Character:FindFirstChild("HumanoidRootPart") and v.Character:FindFirstChild("Humanoid").Health ~= 0 and (not Framework.VisiblityCheck or IsVisible(v.Character.Head.Position, {v.Character, game.Players.LocalPlayer.Character, workspace.CurrentCamera}) == true) then
+                local screenpoint = workspace.CurrentCamera:WorldToScreenPoint(v.Character.HumanoidRootPart.Position)
+                local check = (Vector2.new(mouse.X,mouse.Y)-Vector2.new(screenpoint.X,screenpoint.Y)).magnitude
+                local fovcheck = (Vector2.new(mouse.X,mouse.Y)-Vector2.new(screenpoint.X,screenpoint.Y)).magnitude
+
+                if check < dist then
+                    target  = v
+                    dist = check
+                end
+            end
+        end
+    end
+    
+    return target
+end
 
 local mt = getrawmetatable(game)
 local hookpos
